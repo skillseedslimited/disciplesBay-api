e8d4179e3f836dbb6d3bd049c44c6a92c20f8ed4
checkycheck <checkycheck1256@gmail.com>
2020-11-07 01:41:02 -0600

    fix auth for devotion

--- routers/devotion.routes.js
+++ routers/devotion.routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const DevotionsController = require('../controllers/devotion.controller');
-const { authorize } = require('../middleware/authJwt');
+const { authorize, authorizeUpdated } = require('../middleware/authJwt');
+// const { authorize, authorizeUpdated } = require("../middleware/authJwt");
 
 router
 .route('/:id')
 .get(DevotionsController.getDevotionSingle)
-.delete(authorize('admin'), DevotionsController.deleteDevotion)
-.put(DevotionsController.editDevotion)
+.delete([authorizeUpdated("can-mgt-devotions")], DevotionsController.deleteDevotion)
+.put([authorizeUpdated("can-mgt-devotions")], DevotionsController.editDevotion)
 
 router
 .route('/')
-.post(authorize('admin'), DevotionsController.createDevotion )
+.post([authorizeUpdated("can-mgt-devotions")], DevotionsController.createDevotion )
 .get(DevotionsController.getDevotions)
 
 module.exports = router

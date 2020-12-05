8328b4a402be88ffca18af37435cfc9d8c208a81
checkycheck <checkycheck1256@gmail.com>
2020-12-05 03:56:56 -0600

    fjks

--- config/ffmpeg.config.js
+++ config/ffmpeg.config.js
@@ -16,7 +16,7 @@ const ffmpegconfig = {
             allow_origin: '*'
         },
         trans: {
-            ffmpeg: '/heroku/ffmpeg',
+            ffmpeg: '/bin/ffmpeg',
             tasks: [
                 {
                     app: 'live',

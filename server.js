19b94b61bf34d2c29bedb7a8c981c049d3fad56a
dungji <checkycheck1256@gmail.com>
2020-12-31 12:45:46 -0500

    remove server

--- server.js
+++ server.js
@@ -74,20 +74,20 @@ process.on("unhandledRejection", (err, promise) => {
 const NodeMediaServer = require('node-media-server')
 
 
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60,
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*',
-  },
-}
+// const config = {
+//   rtmp: {
+//     port: 1935,
+//     chunk_size: 60000,
+//     gop_cache: true,
+//     ping: 30,
+//     ping_timeout: 60,
+//   },
+//   http: {
+//     port: 8000,
+//     allow_origin: '*',
+//   },
+// }
 
 
-var nms = new NodeMediaServer(config)
-nms.run()
+// var nms = new NodeMediaServer(config)
+// nms.run()

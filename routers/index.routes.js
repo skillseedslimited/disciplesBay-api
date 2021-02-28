f7c70d789c508952c3c875e738d75fb73060c7f5
dungji <checkycheck1256@gmail.com>
2021-02-28 18:04:23 -0500

    kids content completed

--- routers/index.routes.js
+++ routers/index.routes.js
@@ -22,6 +22,7 @@ module.exports = function (app) {
   );
 
   app.use("/api/v1/sermon", [verifyToken], require("./sermon.routes"));
+  app.use("/api/v1/kids", [verifyToken], require("./kids.routes"));
   app.use("/api/v1/userManagement", require("./userManagement.routes"));
   app.use("/api/v1/counsellor", [verifyToken], require("./counsellor.routes"));
   app.use("/api/v1/wallet", [verifyToken], require("./wallet.routes"));
